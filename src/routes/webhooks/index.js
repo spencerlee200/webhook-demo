00090baea795ffe0e191167d5e35d3027774bc5c
Spencer <SpencersMac@Spencers-MacBook-Pro.local>
2017-03-13 18:52:26 -0400

    adding

--- src/routes/webhooks/index.js
+++ src/routes/webhooks/index.js
@@ -2,7 +2,7 @@ module.exports = (express) => {
   const router = express.Router();
 
   router.get('/dump',(req,res)=> {
-    console.log("Dump: ", req.body)
+    console.log("Dump:", req.body)
     res.json({thanks:true});
   });
 
